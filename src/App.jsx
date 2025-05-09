1d2df25ed1ba2f1fa2eae2c959b71ad8b0fbfa3d
Anshuman <chaoanxuman999@gmail.com>
2025-05-10 00:09:06 +0530

    Routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import {BrowserRouter as Router, Route , Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import './App.css'
-import { LandingPage } from './components/LandingPage';
-import { About} from './components/About';
+import './App.css';
+import { LandingPage } from './components/LandingPage'
+import { About } from './components/About';
+import { Projects } from './components/Projects'
+import { Skills } from './components/Skills'
+import {Socials} from './components/Socials'
+
 
 function App() {
-  <>
-
-  <Router>
-    <Route path='/' element={LandingPage} />
-    <Route path='/about' element={About} />
-  </Router>
-  </>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/socials' element={<Socials />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
