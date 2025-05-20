4de3228fe2ee503dbfe918940430d2e081871363
Anshuman <chaoanxuman999@gmail.com>
2025-05-21 01:27:38 +0530

    FontStyle applied globally

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,7 +6,7 @@ export function LandingPage() {
     return (
         <>
         <div>
-            <p className='italic md:not-italic text-4xl text-blue'>
+            <p className='italic md:not-italic text-4xl text-blue '>
                 Hello, My name is Anshuman 
                 <span className='text-amber-200'>I experties in Backend Web Development</span>
             </p>
